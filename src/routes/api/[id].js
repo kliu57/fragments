6141d298c1302a1ef902713f40a5d9cb9c4880d2
Katie Liu <kliu57@myseneca.ca>
2023-11-10 16:39:33 -0500

    refactored api/[id].js

--- src/routes/api/[id].js
+++ src/routes/api/[id].js
@@ -8,6 +8,25 @@ const path = require('path');
 const markdownIt = require('markdown-it'),
   md = new markdownIt();
 
+// Function to get the data type from an extension
+function getDataType(ext) {
+  if (ext === '.txt') {
+    return 'text/plain';
+  } else if (ext === '.md') {
+    return 'text/markdown';
+  } else if (ext === '.html') {
+    return 'text/html';
+  } else if (ext === '.png' || ext === '.webp' || ext === '.gif') {
+    return `image/${ext}`;
+  } else if (ext === '.jpg' || ext === '.jpeg') {
+    return 'image/jpeg';
+  } else if (ext === '.json') {
+    return 'application/json';
+  } else {
+    return '';
+  }
+}
+
 /**
  * Returns an existing fragment
  */
@@ -17,14 +36,13 @@ module.exports = async (req, res) => {
   let ext = '';
   let convertToType = '';
   let currentType = '';
-  let futureTypes = [];
   let data = '';
 
   // Get id from dynamic value from url
   let id = req.params.id;
 
   // Get extension
-  ext = path.extname(id).substring(1);
+  ext = path.extname(id);
   // Get id without extension
   id = path.parse(id).name;
 
@@ -38,68 +56,38 @@ module.exports = async (req, res) => {
     // Get current type of the fragment
     currentType = fragment.type;
 
-    // Check if extension is provided - meaning we have to convert the fragment
-    if (ext) {
-      // Get the full name of desired conversion type from the extension
-      if (ext === 'txt') {
-        convertToType = 'text/plain';
-      } else if (ext === 'md') {
-        convertToType = 'text/markdown';
-      } else if (ext === 'html') {
-        convertToType = 'text/html';
-      } else if (ext === 'png' || ext === 'webp' || ext === 'gif') {
-        convertToType = `image/${ext}`;
-      } else if (ext === 'jpg' || ext === 'jpeg') {
-        convertToType = 'image/jpeg';
-      } else if (ext === 'json') {
-        convertToType = 'application/json';
-      }
+    // Get the desired conversion data type from the extension
+    convertToType = getDataType(ext);
 
-      // Check if the desired conversion type has a full name
-      if (convertToType != '') {
-        // Check if type is supported
-        if (Fragment.isSupportedType(convertToType)) {
-          // Get list of types the current fragment can be converted into
-          futureTypes = fragment.formats;
+    if (ext === '') {
+      // No extension supplied (no request for conversion)
+      // Return the raw fragment data using the type specified when created (for now this can only be text)
+      res.status(200).json(
+        response.createSuccessResponse({
+          data: data.toString(),
+        })
+      );
+    } else if (Fragment.isSupportedType(convertToType)) {
+      // Extension was supplied and it is a supported data type
 
-          // Check if desired type is one of the types we can convert into
-          if (futureTypes.includes(convertToType)) {
-            let convertedData = data.toString();
+      // Check if desired type is one of the types we can convert into
+      if (fragment.formats.includes(convertToType)) {
+        let convertedData = data.toString();
 
-            if (currentType == 'text/markdown' && convertToType == 'text/html') {
-              // Convert markdown -> html using markdown-it
-              convertedData = md.render(data.toString());
-              logger.debug('Markdown data converted to HTML');
-            }
-
-            // Return the raw fragment data using the desired type (for now this can only be text)
-            res.status(200).json(
-              response.createSuccessResponse({
-                data: convertedData,
-              })
-            );
-          } else {
-            // return HTTP 415 with error message
-            res
-              .status(415)
-              .json(
-                response.createErrorResponse(
-                  415,
-                  `a ${currentType} fragment cannot be returned as a ${convertToType}`
-                )
-              );
-          }
-        } else {
-          console.log('here');
-          // return HTTP 404 with error message
-          res
-            .status(415)
-            .json(
-              response.createErrorResponse(415, 'extension does not represent a supported type')
-            );
+        if (currentType == 'text/markdown' && convertToType == 'text/html') {
+          // Convert markdown -> html using markdown-it
+          convertedData = md.render(data.toString());
+          logger.debug('Markdown data converted to HTML');
         }
+
+        // Return the raw fragment data using the desired type
+        res.status(200).json(
+          response.createSuccessResponse({
+            data: convertedData,
+          })
+        );
       } else {
-        // Type has no full name, so it is unsupported
+        // The desired type is supported but it is not a type that current type can convert into
         // return HTTP 415 with error message
         res
           .status(415)
@@ -111,13 +99,15 @@ module.exports = async (req, res) => {
           );
       }
     } else {
-      // No extension (no request for conversion) or the current type is already the desired type
-      // Return the raw fragment data using the type specified when created (for now this can only be text)
-      res.status(200).json(
-        response.createSuccessResponse({
-          data: data.toString(),
-        })
-      );
+      // return HTTP 415 with error message
+      res
+        .status(415)
+        .json(
+          response.createErrorResponse(
+            415,
+            `a ${currentType} fragment cannot be returned as a ${convertToType}`
+          )
+        );
     }
   } catch (e) {
     // Fragment not found, return HTTP 404 with error message
