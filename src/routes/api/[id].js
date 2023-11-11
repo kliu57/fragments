67b2893582fb1dc98bc2a3f84f0692da43750bf1
Katie Liu <kliu57@myseneca.ca>
2023-11-11 02:10:28 -0500

    changed response returned by get by ID to raw data

--- src/routes/api/[id].js
+++ src/routes/api/[id].js
@@ -39,37 +39,29 @@ module.exports = async (req, res) => {
     currentType = fragment.type;
 
     // Get the desired conversion data type from the extension
-    convertToType = mime.contentType(ext);
+    convertToType = mime.lookup(ext);
 
     if (ext === '') {
       // No extension supplied (no request for conversion)
       // Return the raw fragment data using the type specified when created (for now this can only be text)
-      res.status(200).json(
-        response.createSuccessResponse({
-          headers: { 'Content-Type': currentType },
-          data: data,
-        })
-      );
+      res.setHeader('Content-Type', currentType);
+      res.status(200).send(Buffer.from(data));
     } else if (Fragment.isSupportedType(convertToType)) {
       // Extension was supplied and it is a supported data type
 
       // Check if desired type is one of the types we can convert into
       if (fragment.formats.includes(convertToType)) {
+        logger.debug(`Converting ${currentType} to ${convertToType}...`);
         let convertedData = data;
 
         if (currentType == 'text/markdown' && convertToType == 'text/html') {
           // Convert markdown -> html using markdown-it
           convertedData = md.render(data.toString());
-          logger.debug('Markdown data converted to HTML');
         }
 
         // Return the raw fragment data using the desired type
-        res.status(200).json(
-          response.createSuccessResponse({
-            headers: { 'Content-Type': convertToType },
-            data: convertedData,
-          })
-        );
+        res.setHeader('Content-Type', convertToType);
+        res.status(200).send(Buffer.from(convertedData));
       } else {
         // The desired type is supported but it is not a type that current type can convert into
         // return HTTP 415 with error message
@@ -87,10 +79,7 @@ module.exports = async (req, res) => {
       res
         .status(415)
         .json(
-          response.createErrorResponse(
-            415,
-            `a ${currentType} fragment cannot be returned as a ${convertToType}`
-          )
+          response.createErrorResponse(415, `a ${currentType} fragment  as a ${convertToType}`)
         );
     }
   } catch (e) {
